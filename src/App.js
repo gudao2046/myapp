0b41bef58a02f5629fbaea80c83a58a6c5bc29ba
gudao4006 <gudaoshupi@icloud.com>
2018-06-27 00:29:03 +0800

    add table

--- src/App.js
+++ src/App.js
@@ -2,16 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import {Navbar} from "react-bootstrap";
+import {Navbar,NavDropdown,Nav,NavItem,MenuItem} from "react-bootstrap";
 const navbarInstance = (
     <Navbar>
-        <Navbar.Header>
-            <Navbar.Brand>
-                <a href="#">react-bootstrap</a>
-            </Navbar.Brand>
-        </Navbar.Header>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="#home">React-Bootstrap</a>
+        </Navbar.Brand>
+      </Navbar.Header>
+      <Nav>
+        <NavItem eventKey={1} href="#">
+          Link
+        </NavItem>
+        <NavItem eventKey={2} href="#">
+          Link
+        </NavItem>
+        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+          <MenuItem eventKey={3.1}>Action</MenuItem>
+          <MenuItem eventKey={3.2}>Another action</MenuItem>
+          <MenuItem eventKey={3.3}>Something else here</MenuItem>
+          <MenuItem divider />
+          <MenuItem eventKey={3.4}>Separated link</MenuItem>
+        </NavDropdown>
+      </Nav>
     </Navbar>
 );
+
 
 class App extends Component {
   render() {
@@ -25,7 +41,19 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <table className="App-intro">
+  <tr>
+    <th>Month</th>
+    <th>Savings</th>
+  </tr>
+  <tr>
+    <td>January</td>
+    <td>$100</td>
+  </tr>
+  </table>
+        
       </div>
+      
     );
   }
 }
