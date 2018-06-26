b5cc7d0f5ccf306b38e0677982b6054deff3c1e4
gudao4006 <gudaoshupi@icloud.com>
2018-06-26 23:46:25 +0800

    add react-bootstrap

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {Navbar} from "react-bootstrap";
+const navbarInstance = (
+    <Navbar>
+        <Navbar.Header>
+            <Navbar.Brand>
+                <a href="#">react-bootstrap</a>
+            </Navbar.Brand>
+        </Navbar.Header>
+    </Navbar>
+);
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+      {navbarInstance}
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
